864cb75f19f2b641ec61733c5049acd48359a93e
jaejun <aamoof@gmail.com>
2022-07-13 22:50:05 +0900

    Initial commit

---
+++ src/단어뒤집기/Main.java
@@ -0,0 +1,56 @@
+package 단어뒤집기;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public ArrayList<String> solution(int n, String[] str) {
+//        ArrayList<String> answer = new ArrayList<>();
+//        for (String x : str) {
+//            char[] s = x.toCharArray();
+//            int lt = 0, rt = x.length() - 1;
+//            while (lt < rt) {
+//                char tmp = s[lt];
+//                s[lt] = s[rt];
+//                s[rt] = tmp;
+//                lt++;
+//                rt--;
+//            }
+//            String tmp = String.valueOf(s);
+//            answer.add(tmp);
+//        }
+//        return answer;
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = s.length - 1;
+            while (lt < rt) {
+                char tmp;
+                tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s); //Char배열을 String 표현으로 바꿔
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+
+    }
+}
---
+++ src/특정문자뒤집기_5/Main2.java
@@ -0,0 +1,36 @@
+package 특정문자뒤집기_5;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public String solution(String str) {
+        String answer;
+        char[] s = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(s[lt]))
+                lt++;
+            else if (!Character.isAlphabetic(s[rt]))
+                rt--;
+            else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+
+
+        return String.valueOf(s);
+    }
+
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+
+        System.out.println(T.solution(str));
+
+    }
+}
